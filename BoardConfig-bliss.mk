a2c8d8900d15969ae3e47a828b17c28e1c20c5a1
Nubianprince <evereth@hotmail.com>
2023-01-27 08:46:57 -0500

    blissify

---
+++ BoardConfig-bliss.mk
@@ -0,0 +1,30 @@
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
+# Manifests
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += device/google/gs201/lineage_device_framework_matrix.xml
+DEVICE_MANIFEST_FILE += \
+    device/google/gs201/android.hardware.security.rkp-service.citadel.xml \
+    device/google/gs201/manifest_radio_ds.xml
+
+# Partitions
+AB_OTA_PARTITIONS += \
+    vendor \
+    vendor_dlkm
+
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
+
+# Enable chain partition for vendor.
+BOARD_AVB_VBMETA_VENDOR := vendor
+BOARD_AVB_VBMETA_VENDOR_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_VENDOR_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
+
+AB_OTA_PARTITIONS += \
+    vbmeta_vendor
+
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
---
+++ bliss_common.mk
@@ -0,0 +1,25 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-bliss
+
+# AiAi Config
+PRODUCT_COPY_FILES += \
+    device/google/gs201/allowlist_com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.google.android.as.xml
+
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
+# EUICC
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay
+
+# Face Unlock
+PRODUCT_PACKAGES += \
+    FaceEnrollOverlay \
+    FaceEnrollSettingsOverlay
+
+# PowerShare
+include hardware/google/pixel/powershare/device.mk
+
+# Touch
+include hardware/google/pixel/touch/device.mk
